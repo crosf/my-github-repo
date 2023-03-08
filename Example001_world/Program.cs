7c1dc38b0a7ec39925b07ca115db81472682cfd2
Crosf <Cheshir87@gmail.com>
2023-03-08 20:16:33 +0300

    add example 001 world consloe prroject

---
+++ Example001_world/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("привет как дела");
+Console.WriteLine("привет как дела");
+Console.WriteLine("привет как дела");
+Console.WriteLine("дела");
