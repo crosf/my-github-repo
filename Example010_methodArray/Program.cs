1dd075704db58a6b29813236cc92abff732a2cbf
Crosf <Croaf@mail.ru>
2023-03-18 23:04:35 +0300

    нахождение нучжного числа в масиве

---
+++ Example010_methodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 6, 78, 62, 35, 56, 62 };
+
+int  n = array.Length;
+int find = 62;
+
+int index =0;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
