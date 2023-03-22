92be520e1fa697e4bebf06db079bc96376461913
Crosf <Crosf@mail.ru>
2023-03-22 16:19:39 +0300

    Зодание 2 методов

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор я ");
+}
+// Method1();
+
+
+// Вид 2
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+
+Method2("текст сообшения", 2);
