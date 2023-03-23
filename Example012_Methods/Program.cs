428132040e83c8f8a7ee9422dfcaeba9078db9d5
Crosf <Crosf@mail.ru>
2023-03-23 11:07:24 +0300

    таблица умножения

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -20,7 +20,7 @@ void Method2(string msg, int count)
 }
 
 //Method2(msg:"текст сообшения", count: 2);
- 
+
 // Вид3
 
 int Method3()
@@ -32,17 +32,40 @@ int year = Method3();
 
 // Вид 4
 
-string Method4 (int count, string c)
-{
-    int i = 0;
-    string result = string.Empty;
+// string Method4 (int count, string c)
+// {
+//     int i = 0;
+//     string result = string.Empty;
 
-    while (i<count)
+//     while (i<count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string c)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
     {
         result = result + c;
-        i++;
+      
     }
     return result;
 }
+
 string res = Method4(10, "asdad");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 2 ; j <= 10; j++)   
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
