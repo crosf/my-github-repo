e96dc1c3070b92295bb727c9418a4471f1b8f3c1
Crosf <Crosf@mail.ru>
2023-03-23 10:52:29 +0300

    4 метода

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -19,4 +19,30 @@ void Method2(string msg, int count)
 
 }
 
-Method2("текст сообшения", 2);
+//Method2(msg:"текст сообшения", count: 2);
+ 
+// Вид3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+// Вид 4
+
+string Method4 (int count, string c)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while (i<count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdad");
+Console.WriteLine(res);
