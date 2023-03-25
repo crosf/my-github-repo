e0be9a2a05f15bc2ddcfad5be0c4d2f30c31b793
Crosf <Croaf@mail.ru>
2023-03-25 21:19:37 +0300

    сортировка

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -109,12 +109,52 @@ string Replace(string text, char oldValue, char newValue)
 string newText = Replace(text, ' ', '|');
 
 
-Console.WriteLine(newText);
-Console.WriteLine();
+// Console.WriteLine(newText);
+// Console.WriteLine();
 
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
 
-newText = Replace(newText, 'С', 'с');
-Console.WriteLine(newText);
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+
+int[] arr = { 5, 6, 4, 1, 8, 9, 87, 1, 2, 2 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
+
+
