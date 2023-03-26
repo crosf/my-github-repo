da8754946dacd7a0c1f67374ba84b102c491de1d
Crosf <Croaf@mail.ru>
2023-03-26 23:37:12 +0300

    рекурсии

--- Example013_RecursionAlgirithm/Program.cs
+++ Example013_RecursionAlgirithm/Program.cs
@@ -44,48 +44,84 @@
 
 
 
-int[,] pic = new int[,]
-{
-    {1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,1,1,1,1,0,1},
-    {1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,1,0,0,0,0,1},
-    {1,0,1,1,1,0,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1,1,1,1},
-};
+// int[,] pic = new int[,]
+// {
+//     {1,1,1,1,1,1,1,1,1,1},
+//     {1,0,0,0,0,0,0,0,0,1},
+//     {1,0,0,0,1,1,1,1,0,1},
+//     {1,1,1,1,1,1,1,1,1,1},
+//     {1,0,0,0,1,0,0,0,0,1},
+//     {1,0,1,1,1,0,1,1,1,1},
+//     {1,0,0,0,0,0,0,0,0,1},
+//     {1,1,1,1,1,1,1,1,1,1},
+//     {1,0,0,0,0,0,0,0,0,1},
+//     {1,1,1,1,1,1,1,1,1,1},
+// };
 
 
-void PrintImage(int[,] Image)
-{
-    for (int i = 0; i < Image.GetLength(0); i++)
-    {
-        for (int j = 0; j < Image.GetLength(1); j++)
-        {
-            // Console.Write($"{Image[i, j]} ");
-            if (Image[i, j] == 0) Console.Write($" ");
-            else Console.Write($"+");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintImage(int[,] Image)
+// {
+//     for (int i = 0; i < Image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Image.GetLength(1); j++)
+//         {
+//             // Console.Write($"{Image[i, j]} ");
+//             if (Image[i, j] == 0) Console.Write($" ");
+//             else Console.Write($"+");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-void FillImage(int row, int col)
-{
-    if (pic[row, col] == 0)
-    {
-        pic[row, col] = 1;
-        FillImage(row - 1, col);
-        FillImage(row, col - 1);
-        FillImage(row + 1, col);
-        FillImage(row, col + 1);
-    }
-}
-Console.WriteLine();
-PrintImage(pic);
-FillImage(5,5);
-Console.WriteLine();
-PrintImage(pic);
+// void FillImage(int row, int col)
+// {
+//     if (pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row - 1, col);
+//         FillImage(row, col - 1);
+//         FillImage(row + 1, col);
+//         FillImage(row, col + 1);
+//     }
+// }
+// Console.WriteLine();
+// PrintImage(pic);
+// FillImage(5,5);
+// Console.WriteLine();
+// PrintImage(pic);
+
+
+
+
+
+// Double Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1)+ f(n-2)
+
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
+
