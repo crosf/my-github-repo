cf62444d4bec67b017e2f9dbece91932db13d5f6
Crosf <Crosf@mail.ru>
2023-03-28 16:29:01 +0300

    рекурсии и цыклы

---
+++ Example020_testovy/Program.cs
@@ -0,0 +1,41 @@
+﻿// string NumbersFor(int a, int b) // числа от а до b
+// {
+//     string result = string.Empty;
+//     for (int i = a; i <= b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a < b) return $"{a} " + NumbersRec(a + 1, b);
+//     else return $"{b}";
+// }
+
+// Console.WriteLine(NumbersFor(1, 10));
+// Console.WriteLine(NumbersRec(1, 10));
+
+
+// int SumFor (int n) // сумма числе
+// {
+//     int result = 0;
+//     for (int i = 0; i <= n; i++) result += i;
+//     return result;
+// }
+
+// int SumRec (int n)
+// {
+//     if (n == 0) return 0;
+//     else return n + SumRec(n-1);
+// }
+
+// Console.WriteLine(SumFor(10));
+// Console.WriteLine(SumRec(10));
+
+
+int FactorialFor(int n)
+{
+    int res
+}
