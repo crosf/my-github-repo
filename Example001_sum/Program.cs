11d82dc410ddd664cb360e92b3a4e71a1b59fe80
Crosf <Croaf@mail.ru>
2023-03-31 21:12:42 +0300

    проверка Полного юнити

--- Example001_sum/Program.cs
+++ Example001_sum/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(numberA + numberB);
